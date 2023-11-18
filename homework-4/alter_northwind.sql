e96df196371818217c84eac9684e48a0eaab446c
filippov nikita <filippovnr73@gmail.com>
2023-11-18 23:39:02 +0300

    home_4

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,34 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
 
+ALTER TABLE products ADD CONSTRAINT chk_products_unit_price CHECK (unit_price > 0)
+
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
+
+ALTER TABLE products ADD CONSTRAINT chk_products_discontinued CHECK (discontinued IN (0, 1));
 
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
 
+SELECT * INTO discontinued_products FROM products WHERE discontinued = 1
+
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+
+-- логика какая получилась, сначала удаляем ограничение, удаляем неправильные записи,  удаляем колонку, создаем
+-- (если я правильно понял сериализатор, который перезаполнит id в этой колонке и при дальнейшем удалении строк айди поменяется сам))
+-- заново создаем колонку и вставляем сериализатор, снова делаем привязку
+
+-- вообще не уверен что это то, но по другому придумать не смог...
+
+ALTER TABLE only order_details DROP CONSTRAINT fk_order_details_products;
+DELETE FROM products WHERE discontinued = 1;
+ALTER TABLE products DROP column product_id;
+CREATE SEQUENCE tablename_colname_seq;
+ALTER TABLE products  ADD column product_id integer NOT NULL DEFAULT nextval('tablename_colname_seq');
+ALTER SEQUENCE tablename_colname_seq OWNED BY products.product_id;
+ALTER TABLE ONLY products
+    ADD CONSTRAINT pk_products PRIMARY KEY (product_id);
+select * from products
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,39 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
 
+create table student(
+	student_id serial,
+	first_name varchar(50),
+	last_name varchar,
+	birthday date,
+	phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
+
+ALTER TABLE student add column middle_name varchar(50);
 
 
 -- 3. Удалить колонку middle_name
 
+alter table student drop column middle_name;
+
 
 -- 4. Переименовать колонку birthday в birth_date
+
+ALTER TABLE student RENAME COLUMN birthday TO birth_date;
 
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
 
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32);
+
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('anny', 'smith', '12.12.12', '1488');
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('roma', 'mafia', '11.11.11', '0000');
+INSERT INTO student (first_name, last_name, birth_date, phone) VALUES ('jenaya', 'zabivnoy', '10.10.10', '2000');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+
+TRUNCATE TABLE student;
+
